57dac1bed314bb035c8f0481fbf78b2f2515685d
sjmiller609 <sjmiller609@gmail.com>
2020-03-24 12:23:35 -0400

    Update to 0.12.1

--- variables.tf
+++ variables.tf
@@ -100,7 +100,7 @@ variable "ten_dot_what_cidr" {
 
 variable "astronomer_version" {
   description = "Version of the Astronomer platform installed"
-  default     = "0.12.1-rc.6"
+  default     = "0.12.1"
   type        = string
 }
 
