3d8f62536c6c3ea0970e0629dc1bb8bb21df57af
sjmiller609 <sjmiller609@gmail.com>
2020-03-27 10:49:21 -0400

    Update Airflow chart to 0.12.3 by upgrading Astronomer to 0.12.2

--- variables.tf
+++ variables.tf
@@ -100,7 +100,7 @@ variable "ten_dot_what_cidr" {
 
 variable "astronomer_version" {
   description = "Version of the Astronomer platform installed"
-  default     = "0.12.2"
+  default     = "0.12.3"
   type        = string
 }
 
